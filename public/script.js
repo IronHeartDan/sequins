d75c669fd2b77847670f5b3cd383dc4b454fcf25
Danish Khan <danishkhan.dk2.dk16@gmail.com>
2022-04-27 12:06:15 +0530

    Connected Insta And Nav Indicators

--- public/script.js
+++ public/script.js
@@ -2,6 +2,46 @@ window.onload = (e) => {
   setInterval(() => {
     changeImage();
   }, 3000);
+
+  var urlLoc = location.hash;
+  if (urlLoc) {
+    let deskNavItems = document.querySelectorAll(".desk-nav-item");
+    deskNavItems.forEach((item) => {
+      item.classList.remove("nav_checked");
+    });
+
+    let nav;
+
+    switch (urlLoc) {
+      case "#home":
+        nav = deskNavItems[0];
+        break;
+      case "#products":
+        nav = deskNavItems[1];
+        break;
+      case "#about":
+        nav = deskNavItems[2];
+        break;
+      case "#how":
+        nav = deskNavItems[3];
+        break;
+      default:
+        nav = deskNavItems[0];
+        break;
+    }
+
+    nav.classList.add("nav_checked");
+  }
+
+  var deskNavItems = document.querySelectorAll(".desk-nav-item");
+  deskNavItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      deskNavItems.forEach((elem) => {
+        elem.classList.remove("nav_checked");
+      });
+      item.classList.add("nav_checked");
+    });
+  });
 };
 
 var i = 1;
@@ -19,23 +59,23 @@ function changeImage() {
     case 1:
       setOrder(itemOne, itemTwo, itemThree);
       content.innerHTML =
-        "<strong>Anarkali</strong> - Be the talk of the town!<br class='d-none d-md-block'/>Try our latest range of anarkalis and stand out on every occasion.";
+        "<strong>Nothing tastes as good as looking good feels, we offer the best plazo sets <br /> to compliment your style.</strong><br /> Our collection includes: Anarkali, Sharara, Straight Cut Salwar Suit, Punjabi <br /> Salwar Suit, Palazzo Salwar Suit, Angarkha Salwar Suit.";
       break;
     case 2:
       setOrder(itemThree, itemOne, itemTwo);
       content.innerHTML =
-        "<strong>Plazo Set</strong> - Nothing tastes as good as looking good feels,<br class='d-none d-md-block'/>we offer the best plazo sets to compliment your style.";
+        "<strong>Be the talk of the town! Try our latest range of anarkalis and stand-out <br /> on every occasion.</strong><br /> Our collection includes: Nauvari Saree, Kanjeevaram Saree, Banarasi <br> Saree Bandhani Saree, Chikankari Saree, Drape Saree.";
       break;
     case 3:
       setOrder(itemTwo, itemThree, itemOne);
       content.innerHTML =
-        "<strong>Sharara</strong> - This season spend your money where you can see it hanging around your waist,<br class='d-none d-md-block'/>our beautiful sharars are here to perk up your day.";
+        "<strong>Be the talk of the town! Try our latest range of anarkalis and stand-out <br /> on every occasion.</strong><br />Our collection includes: Nauvari Saree, Kanjeevaram Saree, Banarasi Saree <br /> Bandhani Saree, Chikankari Saree, Drape Saree.";
       break;
 
     default:
       setOrder(itemOne, itemTwo, itemThree);
       content.innerHTML =
-        "<strong>Anarkali</strong> - Be the talk of the town!<br class='d-none d-md-block'/>Try our latest range of anarkalis and stand out on every occasion.";
+        "<strong>Nothing tastes as good as looking good feels, we offer the best plazo sets <br /> to compliment your style.</strong><br /> Our collection includes: Anarkali, Sharara, Straight Cut Salwar Suit, Punjabi <br /> Salwar Suit, Palazzo Salwar Suit, Angarkha Salwar Suit.";
       break;
   }
 
